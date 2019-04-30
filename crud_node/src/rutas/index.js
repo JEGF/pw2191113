52c058a7fe83dffc72e4c161d53bb7ca26d5a882
JEGF <14170932@itculiacan.edu.mx>
2019-04-30 13:58:52 -0600

    Listas de clientes y controlador

--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const rutas = express.Router();
 
+const customerController = require('../controllers/customerController');
 
-rutas.get("/",(req,res) =>{
-	res.send("Hola mundo pero en node jajajajua");
-})
+// rutas.get("/",(req,res) =>{
+	// res.send("Hola mundo pero en node jajajajua");
+// })
+
+rutas.get('/',customerController.list);
+//rutas.post('/add', customerController.delete);
+//rutas.get('/delete', customerController.save);
+//rutas.get('/update/:id', customerController.edit);
+//rutas.post('/update/:id', customerController.update);
+
 module.exports = rutas;
